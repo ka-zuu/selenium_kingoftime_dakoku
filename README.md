4b3d8c2902beda37bcdfaf10446f68578c446e57
ka-zuu <3580752+ka-zuu@users.noreply.github.com>
2021-10-05 22:10:44 +0900

    最初のコミット

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # selenium_kingoftime_dakoku
+
+## 概要
+Seleniumを使って、King of Timeの打刻をする。
+
+## 前提
+* Python 3.8.9で動作確認済み。
+* chromedriverが同じディレクトリにある想定。
+* configファイルにIDとパスワードを記載してある。configファイルは.gitignoreに追記
+
+### configファイル
+```
+[config]
+id={あなたのID}
+pw={あなたのパスワード}
+```
---
+++ dakoku_out.py
@@ -0,0 +1,40 @@
+#webdriverをimportする 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import configparser
+
+# Configファイルを読み込む
+config = configparser.ConfigParser()
+config.read("config", encoding="utf-8")
+kotId = config["config"]["id"]
+kotPw = config["config"]["pw"]
+
+# WebDriver のオプションを設定する
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+print('connectiong to remote browser...')
+Chrome = webdriver.Chrome(executable_path=r"./chromedriver") 
+driver = Chrome
+
+# KoTの打刻ページへアクセス
+driver.get('https://s2.kingtime.jp/independent/recorder/personal/')
+print(driver.current_url)
+
+# ログインIDとパスワードを入力して、OKをクリック
+inputId = driver.find_element(By.ID, "id")
+inputPw = driver.find_element(By.ID, "password")
+buttonOK = driver.find_element_by_css_selector(".btn-control-message")
+inputId.send_keys(kotId)
+inputPw.send_keys(kotPw)
+buttonOK.click()
+
+# 出退勤ボタンが押せるようになるまで待つようにする
+driver.implicitly_wait(5)
+
+# ログインしたら、「退勤」をクリック
+buttonTaikin = driver.find_element_by_css_selector(".record-clock-out")
+buttonTaikin.click()
+
+# ブラウザを終了する
+driver.quit()
